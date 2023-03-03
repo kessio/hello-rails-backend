5f8d021b39359663695c297201a116d0b315a588
unknown <sharonkessio6@gmail.com>
2023-03-03 09:34:06 +0300

    setup db and scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(message: "Hello")
+Greeting.create(message: "Habari Yako")
+Greeting.create(message: "Bonjour")
+Greeting.create(message: "Good Morning")
+Greeting.create(message: "Habari ya Jioni?")
+
